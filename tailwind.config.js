874f3d9ccee7d0bf755e7f01a33526f2596b8dfb
Nihad <85496000+nihadgithub@users.noreply.github.com>
2024-07-04 00:08:55 +0530

    ml font, box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
+      colors: {
         'primary-green': 'hsl(98, 56%, 28%)',
         'light-green': 'hsl(79, 39%, 83%)',
       },
+      boxShadow: {
+        'top-bottom': '0px 12px 12px 1px rgba(0, 0, 0, 0.1), 0px -12px 12px 1px rgba(0, 0, 0, 0.1)',
+      }
     },
   },
   plugins: [],
